f3f3dadd21795a856fc9e10b97457a13833dc666
Hugo Marques <hugodesmarques@gmail.com>
2016-01-24 14:01:38 -0800

    Couse 03 Week 02 Form Validation Exercise

--- conFusion-course3/app/scripts/app.js
+++ conFusion-course3/app/scripts/app.js
@@ -62,4 +62,29 @@ angular.module('confusionApp', [])
       this.isSelected = function (checkTab) {
           return (this.tab === checkTab);
       };
-  });
+  })
+
+  .controller('ContactController', ['$scope', function($scope) {
+    $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+    var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+    $scope.channels = channels;
+    $scope.invalidChannelSelection = false;
+  }])
+
+  .controller('FeedbackController', ['$scope', function($scope) {
+    $scope.sendFeedback = function() {
+        console.log($scope.feedback);
+        if ($scope.feedback.agree && ($scope.feedback.mychannel === "")) {
+          $scope.invalidChannelSelection = true;
+          console.log('incorrect');
+        } else {
+          $scope.invalidChannelSelection = false;
+          $scope.feedback = {mychannel:"", firstName:"", lastName:"",
+                             agree:false, email:"" };
+          $scope.feedback.mychannel="";
+
+          $scope.feedbackForm.$setPristine();
+          console.log($scope.feedback);
+        }
+    };
+  }]);
