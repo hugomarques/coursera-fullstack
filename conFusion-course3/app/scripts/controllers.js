b2ed735b957527ecaa03d87df7e01b77858a32f3
Hugo Marques <hugodesmarques@gmail.com>
2016-02-01 21:28:53 -0800

    Course 03 Week 04 Exercise 02

--- conFusion-course3/app/scripts/controllers.js
+++ conFusion-course3/app/scripts/controllers.js
@@ -8,7 +8,13 @@ angular.module('confusionApp')
             $scope.filtText = '';
             $scope.showDetails = false;
 
-            $scope.dishes= menuFactory.getDishes();
+            $scope.dishes= [];
+            menuFactory.getDishes()
+                .then(
+                    function(response) {
+                        $scope.dishes = response.data;
+                    }
+                );
 
                         
             $scope.select = function(setTab) {
@@ -70,9 +76,14 @@ angular.module('confusionApp')
 
         .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
 
-            var dish= menuFactory.getDish(parseInt($stateParams.id,10));
-            
-            $scope.dish = dish;
+            $scope.dish = {};
+                        menuFactory.getDish(parseInt($stateParams.id,10))
+            .then(
+                function(response){
+                    $scope.dish = response.data;
+                    $scope.showDish=true;
+                }
+            );
             
         }])
 
@@ -94,7 +105,15 @@ angular.module('confusionApp')
         }])
 
         .controller('IndexController', ['$scope', 'menuFactory', 'corporateFactory', function($scope, menuFactory, corporateFactory) {
-            $scope.featuredDish = menuFactory.getDish(0);
+            $scope.dish = {};
+
+            menuFactory.getDish(0)
+                .then(
+                    function(response){
+                        $scope.dish = response.data;
+                        $scope.showDish = true;
+                    }
+                );
             $scope.featuredPromotion = menuFactory.getPromotion(0);
             $scope.leader = corporateFactory.getLeader(3);
 
